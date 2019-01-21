2d7254e226588c20bfab527c18d4e9fbbc026b58
Marcio Barboza <marciocleberbrazil@gmail.com>
2019-01-21 20:37:48 +1100

    Entity Framework Core basic SQLite database

---
+++ ConsoleApp.SQLite/Migrations/20190121091254_NewBlogName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp.SQLite.Migrations
+{
+    public partial class NewBlogName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BlogName",
+                table: "Blogs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BlogName",
+                table: "Blogs");
+        }
+    }
+}
---
+++ ConsoleApp.SQLite/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp.SQLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet", BlogName = "Mocha" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine("{0} - {1}", blog.BlogName, blog.Url);
+                }
+            }
+        }
+    }
+}
