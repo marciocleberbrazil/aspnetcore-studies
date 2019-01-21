9dca3a513585502bdb3754a25cc6e9d388d08195
Marcio Barboza <marciocleberbrazil@gmail.com>
2019-01-21 21:39:52 +1100

    Entity Framework Basic database with Visual Studio and SQL Server

---
+++ EFGetStarted.AspNetCore.NewDb/EFGetStarted.AspNetCore.NewDb/Models/BloggingContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions<BloggingContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
